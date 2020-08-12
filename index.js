7cd807265cd437b8bf049bfde5739a916141c2fc
semira <edyorase@gmail.com>
2020-08-12 04:40:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function happyHolidaysTo (name){
   return (`Happy holidays, ${name}!`)
 }
 function happyCustomHolidayTo (holiday, name){
-  return (`Happy ${holiday},${name}!`)
+  return (`Happy ${holiday}, ${name}!`)
 }
 function holidayCountdown (days,holiday){
   return `It's ${days} days until ${holiday}!`
